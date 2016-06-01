0b9e266eb27dd93531e1d891d80d9f5eb629b004
Sara <given.sara@gmail.com>
2016-05-31 22:21:49 -0700

    create seatalks archive page, loading in the required data from the custom field suite plugin; also renaming of files and slight change to README

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ A custom theme for the future redevelopment [Saturna Island Marine Research and 
 
 I also built the current, static site based on a previous version of the site that had been built in a wysiwyg program. This new theme will use much of the same design but built as a dynamic WordPress site.
 
+The theme is based on Underscores and a boilerplate functionality plugin from Mandi at RED Academy.
---
+++ themes/simres/archive-seatalk.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package simres
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			
+			<p>seatalk archive</p>
+		<?php
+		if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) : the_post(); ?>
+
+			
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						
+						<h4> <?php echo CFS()->get('seatalk_date'); ?>
+						<?php
+							the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+						
+
+						if ( 'post' === get_post_type() ) : ?>
+					
+						<?php
+						endif; ?>
+					</header><!-- .entry-header -->
+
+					<div class="entry-content">
+						
+						<?php $fields = CFS()->get();
+							// echo '<pre>';
+							// print_r($fields);
+							// echo '</pre>'; ?>
+							
+							<?php if(!empty( $fields['presenter'] )): ?>
+								<p> <?php echo $fields['presenter'] ?>
+							<?php endif; ?>
+							
+							<?php if(!empty( $fields['description'] )): ?>
+								<p> <?php echo $fields['description'] ?>
+							<?php endif; ?>
+							
+							<p>
+							<?php if(!empty($fields['time'])): ?>
+								
+								<span class="time"> <?php echo $fields['time']; ?> </span>
+								
+							<?php endif; ?>
+							<?php if(!empty($fields['location'])): ?>
+								
+								<span class="location"> <?php echo $fields['location']; ?> </span>
+								
+							<?php endif; ?>
+							<?php if(!empty($fields['price'])): ?>
+								
+								<span class="price"> <?php echo $fields['price']; ?> </span>
+								
+							<?php endif; ?>
+							
+						 
+					</div><!-- .entry-content -->
+
+					<footer class="entry-footer">
+						<?php simres_entry_footer(); ?>
+					</footer><!-- .entry-footer -->
+				</article><!-- #post-## -->
+
+			<?php endwhile;
+
+
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+
+	get_footer();
