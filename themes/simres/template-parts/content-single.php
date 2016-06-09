7976eb7e242bc67e13fbd51284413953a1f42b08
Sara <given.sara@gmail.com>
2016-06-09 18:41:19 +0000

    style header of default blog posts

--- themes/simres/template-parts/content-single.php
+++ themes/simres/template-parts/content-single.php
@@ -8,40 +8,48 @@
  */
 
 ?>
+<header class="single-standard-header">
+	<?php the_post_thumbnail('full'); ?>
+</header>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+<section class="main-single-standard">
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<p><?php echo get_the_date(); ?></p>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<?php
+					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 
-	<div class="entry-content">
-		<?php
-			the_content();
-		?>
-	</div><!-- .entry-content -->
+			if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<p><?php echo get_the_date(); ?></p>
+			</div><!-- .entry-meta -->
+			<?php
+			endif; ?>
+		</header><!-- .entry-header -->
 
-  <ul class="related">
-    <?php $links = CFS()->get('related_links');
-      foreach( $links as $link ): ?>
-      <li>
-        <?php echo $link['link']; ?>
-      </li>
-    <?php endforeach;?>
-  </ul>
+		<div class="entry-content">
+			<?php
+				the_content();
+			?>
+		</div><!-- .entry-content -->
 
-</article><!-- #post-## -->
+		<?php if(CFS()->get('related_links')): ?>
+	  <ul class="related">
+	    <?php $links = CFS()->get('related_links');
+	      foreach( $links as $link ): ?>
+	      <li>
+	        <?php echo $link['link']; ?>
+	      </li>
+	    <?php endforeach;?>
+	  </ul>
+	<?php endif; ?>
 
-<aside <?php post_class(); ?>>
-  <?php $images = CFS()->get('sidebar_images');
-      foreach( $images as $image): ?>
-      <img src="<?php echo $image['picture']; ?>" alt="" />
-    <?php endforeach; ?>
-</aside>
+	</article><!-- #post-## -->
+
+	<aside <?php post_class(); ?>>
+	  <?php $images = CFS()->get('sidebar_images');
+	      foreach( $images as $image): ?>
+	      <img src="<?php echo $image['picture']; ?>" alt="" />
+	    <?php endforeach; ?>
+	</aside>
+</section>
