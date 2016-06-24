6c76f24b5baba99239b017e6624041e5416fd0f0
Sara <given.sara@gmail.com>
2016-06-24 09:29:10 -0700

    change formatting of the seatalk dates on the front page

--- themes/simres/front-page.php
+++ themes/simres/front-page.php
@@ -48,7 +48,7 @@ get_header(); ?>
 						<a href="<?php echo the_permalink(); ?>">
 							<?php the_post_thumbnail('thumbnail'); ?>
 							<h3><?php the_title(); ?></h3>
-							<p><?php the_date(); ?></p>
+							<p><?php echo date( 'F j, Y', strtotime( CFS()->get('seatalk_date') ) ); ?></p>
 						</a>
 					</li>
 				 <?php endforeach; wp_reset_postdata(); ?>
